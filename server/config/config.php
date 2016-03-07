a8d9e798561d0282c8a9f31c67d6f30c9367e4a9
Happykiller <fabrice.rosito@gmail.com>
2016-03-07 17:01:02 +0100

    Delete config.php

--- server/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = \Oda\SimpleObject\OdaConfig::getInstance();
-$config->urlServer = "http://localhost/server/";
-$config->resourcesPath = "resources/";
-
-//for bd engine
-$config->BD_ENGINE->base = 'abc';
-$config->BD_ENGINE->user = 'abc';
-$config->BD_ENGINE->mdp = 'pass';
